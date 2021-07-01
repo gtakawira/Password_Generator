10703bb77059ee826118530d4b23c99753436297
blacxcellence <tgtakawira@gmail.com>
2021-07-01 13:56:39 -0500

    first draft- prompts

--- script.js
+++ script.js
@@ -5,10 +5,44 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
+//Prompts for  Length and Criteria
+var howLongEl
+function howLong(event){
+  var howLongEl =parseInt( window.prompt('How long do you want the password? Number of characters allowed: 8-128', '8'));
+  //If it meets Length then proceed TO Character type
+  if (howLongEl>= 8 && howLongEl<=128){
+    characterType();
+    return
+  }
+  //second chance to meet length requirement
+  else
+  var howLongEl =parseInt( window.prompt(' Number of characters allowed: 8-128!!!!!', '8'))
+  if (howLongEl>= 8 && howLongEl<=128){
+    characterType();    
+  }
+    //CANCEL
+  else 
+  alert("The generater has been cancelled!!!")
+  
+  //console.log(howLongEl)
+}
+// Prompts for character types
+function characterType() {
+  alert("Select the character types from lowercase, uppercase, numeric, and/or special characters. You have to select at least one ")
+  var lc =confirm("Lowercase?");
+  console.log(lc)
+  var uc =confirm("Uppercase?");
+  console.log(uc)
+  var nm =confirm("Numeric?");
+  console.log(nm)
+  var sc =confirm("Special Characters?");
+  console.log(sc)
+  
+}
+console.log(howLongEl)
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", howLong);
+
