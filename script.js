627919424e296115a6dceb90411e94dbacd9fca7
blacxcellence <tgtakawira@gmail.com>
2021-07-01 20:47:12 -0500

    looping  random picker

--- script.js
+++ script.js
@@ -42,30 +42,56 @@ function characterType() {
   //console.log (howLongEl)
 
   if (lc||uc||nm||sc){
-    compiling()
+    pooling()
   }
   else
-  characterType()
+  alert("You have to select at least one character type. Goodbye!")
+  return
+  
+
+ 
 //password pools
-function compiling(){
+function pooling(){
 var lowercase=["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var uppercase=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 var numeric=["1","2","3","4","5","6","7","8","9","0"]
 var special=["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^","~", "*", "?", ":"]
+var letters=""
 var pool=""
-if (lc){pool=pool+lowercase}
-if (uc){pool=pool+uppercase}
-if (nm) {pool=pool+numeric}
-if (sc) {pool=pool+special}
+if (lc){pool=pool+lowercase;
+ 
+ //making sure each selected character is selected in final word
+  letters= (letters + lowercase[Math.floor(Math.random() * lowercase.length)]);
+}
+if (uc){pool=pool+uppercase
+  letters=( letters + uppercase[Math.floor(Math.random() * uppercase.length)]);
+}
+if (nm) {pool=pool+numeric
+  letters=( letters + numeric[Math.floor(Math.random() * numeric.length)]);
+}
+if (sc) {pool=pool+special
+  letters=( letters + special[Math.floor(Math.random() * special.length)]);
+}
   
 
-
-  
+//generate password from pool 
+console.log(lowercase.characterType)
+console.log(letters)
+console.log(letters.length)
+console.log(howLongEl)
 
  
+ 
+  letters=(letters + pool[Math.floor(Math.random() * pool.length)]);
+
+  for (let i =letters.length; i < howLongEl; i++) { letters=(letters + pool[Math.floor(Math.random() * pool.length)]);
+    
+  } 
   console.log(pool)
-  console.log(lc)
-  console.log(sc)
+  console.log(letters)
+  console.log(letters.length)
+  console.log(howLongEl)
+  
      
   
     
